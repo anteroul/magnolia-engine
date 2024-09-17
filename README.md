a60a8cbc11824d9692d5fe4b5b7c0ee5f7bf2fb2
Antero Lindell <antero.lindell@gmail.com>
2024-09-17 23:49:59 +0300

    v0_3_1

---
+++ README.md
@@ -0,0 +1,10 @@
+# Magnolia WebGL/WebGPU Benchmarker v0.3.1
+### Browser based WebGL/WebGPU benchmarker written in TypeScript.
+
+### Changes:
+- WebGL/WebGPU support
+- Basic "ECS"-style functionality to allow user defined functionality for individual GameObjects.
+
+### TODO:
+- Scenes and SceneManager implementation
+- Optimization for WebGL and WebGPU rendering
--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -25,12 +25,11 @@ export class Renderer {
 
     async init() {
         if (this._ctx instanceof GPUCanvasContext) {
+            // WebGPU initialization:
             if (!navigator.gpu) {
                 console.log("WebGPU not supported on this browser. Switching render mode to WebGL.");
-                this._ctx = <WebGL2RenderingContext>this._canvas.getContext("webgl2");
             } else {
                 console.log("WebGPU support confirmed.");
-                // WebGPU initialization code:
                 this._adapter = <GPUAdapter>await navigator.gpu.requestAdapter();
 
                 if (!this._adapter) {
@@ -48,20 +47,22 @@ export class Renderer {
 
                 this._ctx.configure({
                     device: <GPUDevice>this._device,
-                    format: this._textureFormat
+                    format: this._textureFormat,
                 });
 
-                this._pipeline = await createRenderPipeline(this._device, <GPUShaderModule>await this.shaderLoader.load("./src/shaders/triangle.wgsl"));
-                // initialization finished
+                this._pipeline = await createRenderPipeline(
+                    this._device,
+                    <GPUShaderModule>(
+                        await this.shaderLoader.load("./src/shaders/triangle.wgsl")
+                    )
+                );
             }
+            // initialization finished
         } else {
-            // WebGL initialization
+            // WebGL initialization:
             if (!this._ctx) {
-                this._ctx = <WebGLRenderingContext>this._canvas.getContext("webgl2");
-                if (!this._ctx) {
-                    console.log("Failed to initialize WebGL2. Switching to legacy WebGL.");
-                    this._ctx = <WebGLRenderingContext>this._canvas.getContext("webgl");
-                }
+                console.log("Failed to initialize WebGL2. Switching to legacy WebGL.");
+                this._ctx = <WebGLRenderingContext> this._canvas.getContext("webgl");
             }
             this._shaderProgram = await this.shaderLoader.load("./src/shaders/triangle.glsl");
             // initialization finished
@@ -70,14 +71,17 @@ export class Renderer {
     }
 
     render() {
+        // WebGPU rendering:
         if (this.currentAPI === "WebGPU") {
             const renderPassDescriptor = <GPURenderPassDescriptor>{
-                colorAttachments: [{
-                    view: this.ctx.getCurrentTexture().createView(),
-                    loadOp: 'clear',
-                    storeOp: 'store',
-                    clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 }
-                }]
+                colorAttachments: [
+                    {
+                        view: this.ctx.getCurrentTexture().createView(),
+                        loadOp: "clear",
+                        storeOp: "store",
+                        clearValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+                    },
+                ],
             };
 
             const commandEncoder = this.device.createCommandEncoder();
@@ -86,7 +90,11 @@ export class Renderer {
 
             this.renderQueue.forEach((renderable) => {
                 const uniformData = this.getUniformDataForRenderable(renderable).buffer;
-                this.device.queue.writeBuffer(<GPUBuffer>renderable.uniformBuffer, 0, uniformData);
+                this.device.queue.writeBuffer(
+                    <GPUBuffer>renderable.uniformBuffer,
+                    0,
+                    uniformData
+                );
                 const bindGroup = this.device.createBindGroup({
                     layout: <GPUBindGroupLayout>this._pipeline?.getBindGroupLayout(0),
                     entries: [
@@ -106,17 +114,18 @@ export class Renderer {
             passEncoder?.end();
             this.device.queue.submit([commandEncoder.finish()]);
         } else {
+            // WebGL rendering:
             const program = this.glProgram;
             this.ctxGL.clearColor(0.0, 0.0, 0.0, 1.0);
             this.ctxGL.clear(this.ctxGL.COLOR_BUFFER_BIT);
 
-            this.renderQueue.forEach(renderable => {
+            this.renderQueue.forEach((renderable) => {
                 this.setPositionAttribute(this.ctxGL, renderable);
                 this.setColorAttribute(this.ctxGL, renderable);
                 this.ctxGL.useProgram(program);
 
                 const translationMatrix = mat3.create();
-                
+
                 mat3.translate(
                     translationMatrix,
                     translationMatrix,
@@ -126,7 +135,7 @@ export class Renderer {
                 this.ctxGL.uniformMatrix3fv(
                     this.ctxGL.getUniformLocation(program, "uTranslationMatrix"),
                     false,
-                    translationMatrix,
+                    translationMatrix
                 );
 
                 this.ctxGL.drawArrays(this.ctxGL.TRIANGLES, 0, renderable.vertexCount);
@@ -136,9 +145,9 @@ export class Renderer {
 
     getUniformDataForRenderable(renderable: Renderable): { buffer: ArrayBuffer } {
         const uniformData = new Float32Array(16);
-        uniformData.set(renderable.position, 0);              // set the position
-        uniformData.set(renderable.scale, 2);                 // set the scale
-        uniformData.set(renderable.color, 4);                 // set the color
+        uniformData.set(renderable.position, 0); // set the position
+        uniformData.set(renderable.scale, 2); // set the scale
+        uniformData.set(renderable.color, 4); // set the color
 
         renderable.uniformBuffer = this.device.createBuffer({
             label: "triangle uniform",
@@ -146,13 +155,18 @@ export class Renderer {
             usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
             mappedAtCreation: true,
         });
-        new Float32Array(renderable.uniformBuffer.getMappedRange()).set(uniformData);
+        new Float32Array(renderable.uniformBuffer.getMappedRange()).set(
+            uniformData
+        );
         renderable.uniformBuffer.unmap();
         // Populate `uniformData` as needed for each renderable
         return { buffer: uniformData.buffer };
     }
 
-    setPositionAttribute(ctx: WebGL2RenderingContext | WebGLRenderingContext, r: Renderable) {
+    setPositionAttribute(
+        ctx: WebGL2RenderingContext | WebGLRenderingContext,
+        r: Renderable
+    ) {
         const numComponents = 2; // pull out 2 values per iteration
         const type = ctx.FLOAT; // the data in the buffer is 32bit floats
         const normalize = false; // don't normalize
@@ -167,28 +181,39 @@ export class Renderer {
             type,
             normalize,
             stride,
-            offset,
+            offset
         );
-        ctx.enableVertexAttribArray(ctx.getAttribLocation(this.glProgram, "aVertexPosition"));
+        ctx.enableVertexAttribArray(
+            ctx.getAttribLocation(this.glProgram, "aVertexPosition")
+        );
     }
 
-    setColorAttribute(ctx: WebGL2RenderingContext | WebGLRenderingContext, r: Renderable) {
+    setColorAttribute(
+        ctx: WebGL2RenderingContext | WebGLRenderingContext,
+        r: Renderable
+    ) {
         const numComponents = 4;
         const type = ctx.FLOAT;
         const normalize = false;
         const stride = 0;
         const offset = 0;
         ctx.bindBuffer(ctx.ARRAY_BUFFER, ctx.createBuffer());
-        ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(r.colorData), ctx.STATIC_DRAW);
+        ctx.bufferData(
+            ctx.ARRAY_BUFFER,
+            new Float32Array(r.colorData),
+            ctx.STATIC_DRAW
+        );
         ctx.vertexAttribPointer(
             ctx.getAttribLocation(this.glProgram, "aVertexColor"),
             numComponents,
             type,
             normalize,
             stride,
-            offset,
+            offset
         );
-        ctx.enableVertexAttribArray(ctx.getAttribLocation(this.glProgram, "aVertexColor"));
+        ctx.enableVertexAttribArray(
+            ctx.getAttribLocation(this.glProgram, "aVertexColor")
+        );
     }
 
     get ctx() {
@@ -210,11 +235,12 @@ export class Renderer {
     get currentAPI() {
         if (this._ctx instanceof GPUCanvasContext)
             return "WebGPU";
-        if (this._ctx instanceof WebGL2RenderingContext)
-            return "WebGL2";
-        if (this._ctx instanceof WebGLRenderingContext)
+        else if (this._ctx instanceof WebGL2RenderingContext)
             return "WebGL";
-        return "none";
+        else if (this._ctx instanceof WebGLRenderingContext)
+            return "Legacy WebGL";
+        else
+            return "none";
     }
 
     get geometryCount() {
