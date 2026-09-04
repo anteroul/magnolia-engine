b69ac9d4fc994b96f09dce40d961b12a66ac697d
valiant <antero.lindell@gmail.com>
2026-09-04 11:42:41 +0300

    minor changes

--- src/ui.ts
+++ src/ui.ts
@@ -1,10 +1,10 @@
-import { API, currentAPI } from './core/render_modes';
+import { API } from './core/render_modes';
 
 export function setupUI(onSettingsChanged: (api: API, triangles: number) => void) {
     const apiSelect = document.getElementById("apiSelect") as HTMLSelectElement;
     const geometrySlider = document.getElementById("geometryCount") as HTMLInputElement;
     const applyButton = document.getElementById("applySettings") as HTMLButtonElement;
-    let selectedAPI = <API> currentAPI;
+    let selectedAPI = apiSelect.value;
 
     applyButton.addEventListener("click", () => {
         if (apiSelect.value === "webgl") selectedAPI = API.WEBGL;
